96771ae83d6eb01973cab5f8067207dd64ebf7b6
kamipatel <kamipatel@gmail.com>
2019-04-26 11:07:27 -0500

    update

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ const ORGUSERNAME = process.env.orgusername;
 const ORGPASSWORD = process.env.orgpassword;
 const ORLURL = process.env.orgurl;
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.send("CreditCheck App");
 });
 
 app.get("/CreditScore", async (req, res) => {
-
   const conn = await logintoOrgAndSendEvents();
 
   res.send({
@@ -22,53 +21,52 @@ app.get("/CreditScore", async (req, res) => {
   });
 
   console.log("Credit status complete!");
-
 }); //CreditScore() end
 
 app.listen(PORT);
 
 async function logintoOrgAndSendEvents() {
-
   console.log("logintoOrgAndSendEvents called");
 
   var username = ORGUSERNAME;
   //var password = 'RMe]w%0c67';
-  var password = 'RMe]w%0c678didUBiMn6f5WCG1pxUgtq7Z';
-
+  var password = ORGPASSWORD;
 
   var conn = new jsforce.Connection({
-    loginUrl: 'https://test.salesforce.com'
+    loginUrl: ORLURL
   });
 
   await conn.login(username, password);
   await sleep(8000);
-  await SendStatusEvent(conn, 'Checking Experian')
+  await SendStatusEvent(conn, "MayBeExperianNot");
   await sleep(2000);
-  await SendStatusEvent(conn, 'Equifax')
+  await SendStatusEvent(conn, "MayBeEquifaxNot");
   await sleep(2000);
-  await SendStatusEvent(conn, 'TransUnion')
+  await SendStatusEvent(conn, "MayBeTransUnion");
   await sleep(3000);
 
   console.log("logintoOrg Complete");
 
   return conn;
-
 }
 
 async function SendStatusEvent(conn, msg) {
   console.log("SendStatusEvent called!");
-  await conn.sobject("CreditStatus__e").create({
-    message__c: msg
-  }, function (err, ret) {
-    if (err || !ret.success) {
-      return console.error(err, ret);
+  await conn.sobject("CreditStatus__e").create(
+    {
+      message__c: msg
+    },
+    function(err, ret) {
+      if (err || !ret.success) {
+        return console.error(err, ret);
+      }
+      console.log("SendStatusEvent complete");
     }
-    console.log("SendStatusEvent complete");
-  })
-};
+  );
+}
 
 function sleep(ms) {
   return new Promise(resolve => {
-    setTimeout(resolve, ms)
-  })
+    setTimeout(resolve, ms);
+  });
 }
