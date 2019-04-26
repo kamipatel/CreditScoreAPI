5fed75b94698e1ef0257d320d7389b00e0fb9fb6
kamipatel <kamipatel@gmail.com>
2019-04-26 09:54:53 -0500

    Added a Procfile.

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.listen(PORT)
